a938f79bee98257af55b1824def7ea7ff67e80e3
yuzfu <rfu@smith.edu>
2025-02-18 00:16:29 -0500

    hw3

--- Hwks/Hw3/technical/js/slideshow.js
+++ Hwks/Hw3/technical/js/slideshow.js
@@ -1,28 +1,56 @@
+// Template function
+const number = ["1 / 3", "2 / 3", "3 / 3"];
+const image = ["images/html.jpg", "images/css.jpg", "images/javascript.jpg"];
+const text = ["HTML", "CSS", "JavaScript"];
+
+const TEMPLATE = `<div class="mySlides fade">
+                    <div class="numbertext">NUMBER</div>
+                    <img src="IMAGE">
+                    <div class="text">TEXT</div>
+                </div>`;
+
+let slidesHTML = "";
+
+for (let i = 0; i < number.length; i++) {
+    let slideContent = TEMPLATE
+        .replace("NUMBER", number[i])
+        .replace("IMAGE", image[i])
+        .replace("TEXT", text[i]);
+
+    slidesHTML += slideContent;
+}
+
+document.getElementById("slides").innerHTML = slidesHTML;
+
+// Slideshow control
 let slideIndex = 1;
-            showSlides(slideIndex);
+let timer = null;
+showSlides(slideIndex);
 
-            // Next/previous controls
-            function plusSlides(n) {
-            showSlides(slideIndex += n);
-            }
+function plusSlides(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex += n);
+}
 
-            // Thumbnail image controls
-            function currentSlide(n) {
-            showSlides(slideIndex = n);
-            }
+function currentSlide(n) {
+    clearTimeout(timer);
+    showSlides(slideIndex = n);
+}
 
-            function showSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";
-            dots[slideIndex-1].className += " active";
-            }
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    timer = setTimeout(showSlides, 2000);
+}
+
